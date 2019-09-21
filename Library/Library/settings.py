533f95e6bd983688c7d91811f233df33ecdf296c
ZhivkoZaikov <zhivkozaikov@gmail.com>
2019-09-21 17:18:55 +0300

    Second Lap

--- Library/Library/settings.py
+++ Library/Library/settings.py
@@ -80,6 +80,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'BookProject',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'pass1234',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
